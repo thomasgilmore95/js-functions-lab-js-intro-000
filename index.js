826122c63e9b111541630bf65259f73019a1ce28
Thomas Gilmore <tomtitans@aol.com>
2019-10-28 20:17:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // write your code below!
 function happyHolidays() {
-  return "Happy holidays!";
+  return "Happy holidays!"
 }
 
-function happyHolidaysTo() {
-  
+function happyHolidaysTo(name) {
+  `Happy holidays, ${name}!`
 }
